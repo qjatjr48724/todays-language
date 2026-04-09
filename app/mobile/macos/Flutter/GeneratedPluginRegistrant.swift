b5b3317e52603aa55f99993f3c22c3cf39d57b80
kim beom seok <qjatjr1285@naver.com>
2026-04-10 03:37:06 +0900

    chore(macos): 플러그인 레지스트리 갱신 google_sign_in / sign_in_with_apple 추가에 따른 자동 생성 파일 업데이트

--- app/mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,14 @@ import cloud_firestore
 import cloud_functions
 import firebase_auth
 import firebase_core
+import google_sign_in_ios
+import sign_in_with_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FirebaseFunctionsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
 }
